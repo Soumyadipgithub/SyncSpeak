546ea37804bf81c004ea6fd0510823af440ae1c9
Soumyadipgithub <122915461+Soumyadipgithub@users.noreply.github.com>
2026-04-23 00:27:41 +0530

    fix: resolve vb-cable installation and update ui (#47)

--- src/renderer/constants.ts
+++ src/renderer/constants.ts
@@ -1,9 +1,7 @@
-import pkg from '../../package.json';
-
 /**
  * Single Source of Truth for Sync Speak branding and versioning.
- * Changing values here or in package.json will update the entire application.
+ * Changing values here will update the entire application.
  */
 export const APP_NAME = 'Sync Speak';
-export const APP_VERSION = pkg.version;
+export const APP_VERSION = '3.0.3';
 export const GITHUB_URL = 'https://github.com/Soumyadipgithub/SyncSpeak';
